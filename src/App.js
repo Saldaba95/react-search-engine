ab9049c4b75430a41d0c0912fb9932e369ffa5e0
Saldaba95 <aldaba.sarei1995@gmail.com>
2024-05-25 19:41:39 -0500

    Update App.js
    added links

--- src/App.js
+++ src/App.js
@@ -12,11 +12,19 @@ function App() {
         <p>
           Let's stay connected... <br />
           Check out my{" "}
-          <a className="Github" href="" target="blank">
+          <a
+            className="Github"
+            href="https://github.com/Saldaba95/react-search-engine"
+            target="blank"
+          >
             GitHub
           </a>{" "}
           and{" "}
-          <a className="Shecodes" href="" target="blank">
+          <a
+            className="Shecodes"
+            href="https://www.shecodes.io/graduates/66500-sarei-aldaba"
+            target="blank"
+          >
             SheCodes
           </a>
           .
