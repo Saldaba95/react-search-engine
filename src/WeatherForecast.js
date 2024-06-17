a0054164dd9308b9a3ff2f54f028fec0289e6305
Saldaba95 <aldaba.sarei1995@gmail.com>
2024-06-16 22:01:53 -0500

    Update WeatherForecast.js
    tired to get it to work when loaded still no luck

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,14 +17,14 @@ export default function WeatherForecast(props) {
       <div className="WeatherForecast">
         <div className="row">
           <div className="col">
-            <div className="WeatherForecast-day">{forecast[4].dt}</div>
-            <WeatherIcon code={forecast[4].weather[4].icon} size={36} />
+            <div className="WeatherForecast-day">sun </div>
+            <WeatherIcon code="01d" size={36} />
             <div className="WeatherForecast-temperature">
               <span className="WeatherForecast-temperature-max">
-                {Math.round(forecast[4].temp.max)}°
+                {forecast[0].temp.max} °
               </span>
               <span className="WeatherForecast-temperature-min">
-                {Math.round(forecast[4].temp.min)} °
+                {forecast[0].temp.min} °
               </span>
             </div>
           </div>
@@ -35,7 +35,7 @@ export default function WeatherForecast(props) {
     let apiKey = "616b14cbd38253313b3b8852fa77335d";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid=${apiKey}$units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}$units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
 
